a2a2cb813b68746bb19e0242fac91a45ca18277e
UHPD Games <uhpdgames@gmail.com>
2017-09-25 10:55:16 +0700

    gitlab.com/uhpdgames

--- module/descv/trinhchinhsua.php
+++
@@ -1,6 +0,0 @@
-<?php
-function trinhchinhsua_cv() {
-    $a =  file_get_contents('http://drupal.uit/sites/all/modules/descv/descv_test.php');
-    return '<div id="edit-cv" next-id="">'.$a.'</div>';
-}
-?>
